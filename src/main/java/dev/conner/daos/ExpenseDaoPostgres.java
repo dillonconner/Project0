a82370f319578f62306ef815cbf51450a46db925
dillonconner <dillonconner@gmail.com>
2022-08-05 13:44:00 -0400

    employee functionality check

--- src/main/java/dev/conner/daos/ExpenseDaoPostgres.java
+++ src/main/java/dev/conner/daos/ExpenseDaoPostgres.java
@@ -97,6 +97,9 @@ public class ExpenseDaoPostgres implements ExpenseDAO{
     @Override
     public Set<Expense> getAllExpensesByQuery(String param, List<String> val) {
         try(Connection conn = ConnectionUtil.createConnection()){
+
+            //////////////       DONT LOOK AT THIS RN IM PLAYING WITH IT STILL  ////////////////////////////////
+
             //get empty table
             String checkTable = "select * from expense where 1 = 2";
             PreparedStatement checkPs = conn.prepareStatement(checkTable);
